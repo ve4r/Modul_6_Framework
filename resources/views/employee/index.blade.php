413d6280fe78862eeadc02cf751d8f6e46b6db96
ve4r <133086697+ve4r@users.noreply.github.com>
2023-05-31 19:04:15 +0700

    Update index.blade.php

--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -33,7 +33,7 @@
                             <td>{{ $employee->email }}</td>
                             <td>{{ $employee->age }}</td>
                             <td>{{ $employee->position->name }}</td>
-                            <td>@include('employee.actions')</td>
+                            <td>@include('employee.action')</td>
                         </tr>
                     @endforeach
                 </tbody>
